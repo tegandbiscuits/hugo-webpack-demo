547079c48a16b67155489034eaed7c14ce974793
Tegan Rauh <tegan.rauh@shiftdsm.com>
2020-03-09 10:26:55 -0500

    Bundle CSS with webpack and setup Autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: [path.resolve('src', 'js', 'app.js')],
+  entry: [
+    path.resolve('src', 'js', 'app.js'),
+    path.resolve('src', 'styles', 'app.css'),
+  ],
   output: {
     path: path.resolve('static', 'assets'),
     filename: 'bundle.js',
@@ -13,6 +17,15 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
     ],
   },
+  plugins: [new MiniCssExtractPlugin()],
 };
